72539bc1f4aced1296eda4f657ccee65f0dc856f
jwkeena <jwkeena@gmail.com>
2019-08-14 14:49:05 -0400

    Boilerplate routing

---
+++ routes/api/games.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const gamesController = require("../../controllers/gamesController");
+
+// Matches with "/api/books"
+router.route("/")
+  .get(gamesController.findAll)
+  .post(gamesController.create);
+
+// Matches with "/api/books/:id"
+router
+  .route("/:id")
+  .get(gamesController.findById)
+  .put(gamesController.update)
+  .delete(gamesController.remove);
+
+module.exports = router;
