36cc1554125e36fcb159378c51751149332a3df5
CaptainEFFF <davidbanville4@gmail.com>
2019-08-15 20:43:52 -0400

    added ripple

--- client/src/components/Painting/index.js
+++ client/src/components/Painting/index.js
@@ -1,14 +1,13 @@
 import * as React from 'react';
-import { render } from 'react-dom';
-// import GithubCorner from 'react-github-corner';
-// import GradientButton from 'react-linear-gradient-button';
+// import { render } from 'react-dom';
+
 
 import './index.css';
 import WaterWave from '../../index';
 
 const image = './bobomb.jpg';
 
-const Demo = () => {
+const Painting = () => {
   return (
     <WaterWave
       style={{ top: '-50', left: '-50', width: '100%', height: '100%' }}
@@ -18,5 +17,4 @@ const Demo = () => {
     </WaterWave>
   );
 };
-
-render(<Demo />, document.querySelector('#demo'));
+export default Painting
