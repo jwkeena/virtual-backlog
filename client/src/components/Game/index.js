cfd38bb41e4dd73b49a9f510bb092e0e03140d35
CaptainEFFF <davidbanville4@gmail.com>
2019-08-27 21:06:26 -0400

    added colors, nav tabs, and started toggles

--- client/src/components/Game/index.js
+++ client/src/components/Game/index.js
@@ -4,25 +4,50 @@ import React from 'react';
 // // return { init : init };
 
  function Game(props) {
-   return ( <li key={props.id} className={(props.zIndex === 1  && props.clicked ? 'z-index'+props.zCounter + 1 : 'z-index'+props.zCounter)} onClick = {props.handleClick}>
-    <div className={(props.gameOpen === 1 && props.clicked ? 'bk-game game ' + props.id +' bk-outside'  : props.gameOpen === 2 && props.clicked  ? 'bk-game game '+props.id+' bk-outside bk-viewinside' : props.gameOpen === 3 && props.clicked ? 'bk-game game '+props.id+' bk-outside bk-viewinside bk-open' :'bk-game game'+ props.id)}> 
+   return ( <li key={props._id} className={(props.zIndex === 1  && props.clicked ? 'z-index'+props.zCounter + 1 : 'z-index'+props.zCounter)} onClick = {props.handleClick}>
+    <div className={(props.gameOpen === 1 && props.clicked ? 'bk-game game' + props._id +' bk-outside'  : props.gameOpen === 2 && props.clicked  ? 'bk-game game'+props._id+' bk-outside bk-viewinside' : props.gameOpen === 3 && props.clicked ? 'bk-game game'+props._id+' bk-outside bk-viewinside bk-open' :'bk-game game'+ props._id)}> 
    
     <div className='bk-front'>
         <div className='bk-cover-back'></div>
-        <div className='bk-cover' src={props.box_art}>
+        <div className={('bk-cover '+ props.system_type )} src={(props.box_art)}>
             <h2><span>  {props.title}  </span> <span> {props.system_type} </span></h2>   
         </div>
     </div>
 
     <div className='bk-page'>
-        <div className='bk-content bk-content-current'>
+        <div className={(props.page === 1 && props.clicked ? 'bk-content bk-content-current': 'bk-content')}>
             <p>{props.description}</p>
         </div>
-        <div className='bk-content'>
-            <p>{props.is_physical}</p>
-            <p>{props.is_beaten}</p>
+        <div className={(props.page === 2 && props.clicked ? 'bk-content bk-content-current': 'bk-content')}>
+            <p>Owned?{props.owned}</p>
+            <p>Physical Copy?{props.is_physical}</p>
+            <p>Completed?{props.is_beaten}</p>
+            <p>Currently Playing?{props.now_playing}</p>
+            <p>Favorite Game?{props.is_favorite}</p>
+            <p>Rating{props.rating}</p>
+            <p>Estimated Value{props.price}</p>
+            {/* <div className="custom-control custom-switch">
+            <input type="checkbox" className="custom-control-input" id="customSwitches"/>
+            <label className="custom-control-label" htmlFor="customSwitches">Toggle this switch element</label>
+            </div> */}
         </div>
+        <div className={(props.page === 3 && props.clicked ? 'bk-content bk-content-current': 'bk-content')}>
+            <p>Notes{props.note}</p>
+            <p>Year Released{props.date}</p>
+        </div>
+        <div className={(props.page === 3 && props.clicked ? 'bk-content bk-content-current': 'bk-content')}>
+            <p>Delete This Title?{props.note}</p>
+            
+        </div>
+
+    <nav>
+    <span className="bk-page-prev" onClick = {props.handlePageLeft}>&lt;</span>
+    <span className="bk-page-close" onClick = {props.handleClose}>X</span>
+    <span className="bk-page-next" onClick = {props.handlePageRight}>&gt;</span>
+    </nav>
+
     </div>
+
 
     <div className='bk-back'>
         <p>{props.title} </p>
@@ -31,7 +56,7 @@ import React from 'react';
 
     <div className='bk-right'></div>
 
-    <div className='bk-left'>
+    <div className={('bk-left ' + props.system_type)}>
         <h2><span>{props.title}</span><span>{props.system_type}</span></h2>
     </div>
 
--- client/src/pages/Games.js
+++ client/src/pages/Games.js
@@ -1,21 +1,11 @@
 import React, { Component } from "react";
-import { MDBContainer, MDBRow, MDBCol } from 'mdbreact';
+import { MDBContainer, MDBRow, MDBCol} from 'mdbreact';
 import FixedNavbar from "../components/FixedNavbar";
 import Game from "../components/Game";
 import API from "../utils/API";
 import gameSeed from "../components/GamesTest/gameListTest";
 import '../components/Game/styles.css'
-// import Modernizr from '../utils/modernizr.custom'
 
-// var transEndEventNames = {
-//     'WebkitTransition' : 'webkitTransitionEnd',
-//     'MozTransition' : 'transitionend',
-//     'OTransition' : 'oTransitionEnd',
-//     'msTransition' : 'MSTransitionEnd',
-//     'transition' : 'transitionend'
-// }
-// var transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ]
-// gameList = $( '#bk-list > li > div.bk-game' ), gamesCount = 13, currentgame = -1, counter = 0; 
 
 class Games extends Component {
     constructor(props) {
@@ -28,7 +18,12 @@ class Games extends Component {
             counter: 0,
             gameOpen: 0,
             clicked: [],
-            checked: false
+            checked: false,
+            zIndex: 0,
+            zCounter: 0,
+            page:1,
+            switch1:true,
+            switch2:false
         }
         this.loadGames = this.loadGames.bind(this);
     }
@@ -39,7 +34,7 @@ class Games extends Component {
     
     handleClick = i =>{
         let clickedArray = this.state.clicked.slice(0)
-        console.log("clickedArray" + clickedArray)
+        // console.log("clickedArray" + clickedArray)
         
         
         if (this.state.gameOpen === 0){
@@ -62,7 +57,28 @@ class Games extends Component {
 
         }
 
-        else if (this.state.gameOpen === 3){
+        
+    
+        //     // $content.removeClass( 'bk-content-current').eq( this.state.current ).addClass( 'bk-content-current' );
+        // }
+
+    }
+
+    handlePageRight = i => {
+        console.log("clicked!")
+        if (this.state.page === 1){
+            this.setState({page:2})
+        }
+        else if (this.state.page === 2){
+            this.setState({page:3})
+        }  
+        else if (this.state.page === 3){
+            this.setState({page:4})
+        }  
+    }
+    handleClose = i => {
+        console.log("clicked!")
+         if (this.state.gameOpen === 3){
             this.setState({gameOpen:2})
 
             setTimeout(() => {
@@ -72,15 +88,30 @@ class Games extends Component {
             setTimeout(() => {
             this.setState({gameOpen:0})
             this.setState({clicked:[]})
-            }, 1000)
             this.setState({zIndex: 0})
-            
+            this.setState({page:1})
+            }, 1000)
         }
-    
-        //     // $content.removeClass( 'bk-content-current').eq( this.state.current ).addClass( 'bk-content-current' );
-        // }
-
     }
+    handlePageLeft = i => {
+        console.log("clicked!")
+        if (this.state.page === 2){
+            this.setState({page:1})
+        }
+        else if (this.state.page === 3){
+            this.setState({page:2})
+        }  
+        else if (this.state.page === 4){
+            this.setState({page:3})
+        }  
+    }
+
+    handleSwitchChange = nr => () => {
+        let switchNumber = `switch${nr}`;
+        this.setState({
+          [switchNumber]: !this.state[switchNumber]
+        });
+      }
 
     loadGames () {
         const username = localStorage.getItem("username");
@@ -104,11 +135,11 @@ class Games extends Component {
         
         <br/>
         
-            {(this.state.gamesLoaded) && 
+            {/* {(this.state.gamesLoaded) &&  */}
             
             <MDBRow>
                 <MDBCol size='sm-12' className = 'bk-list'>
-                    {this.state.gamesLoaded.map((games,i) => 
+                    {this.state.gameSeed.map((games,i) => 
                      <Game 
                     gameOpen = {this.state.gameOpen} 
                     title = {games.title}
@@ -125,19 +156,27 @@ class Games extends Component {
                     date = {games.data}
                     note = {games.note}
                     id = {games._id}
+                    page = {this.state.page}
+                    switch1 = {this.state.switch1}
+                    switch2 = {this.state.switch2}
                     zIndex = {this.state.zIndex}
                     clicked = {this.state.clicked[i]}
                     zCounter = {zCounter < gamesCount/2 ? (zCounter += 1): gamesCount - zCounter && negativeC --}
                     handleClick = { () => this.handleClick(i)}
+                    handlePageLeft = {()=>this.handlePageLeft(i)}
+                    handleClose = {() => this.handleClose(i)}
+                    handlePageRight = {()=>this.handlePageRight(i)}
+                    handleSwitchChange = {()=>this.handleSwitchChange()}
                     />)}
-                    
+                    {/* <MDBSwitch checked={this.state.switch1} onChange={this.handleSwitchChange(1)} /> */}
                 </MDBCol>
             </MDBRow>
-            }
             </MDBContainer> 
+            
         )
     }
     
-};
+}
+;
 
 export default Games;
