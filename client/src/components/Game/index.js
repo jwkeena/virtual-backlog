d5efcc70470b272f10db1fe4b9a9d52f7e9aa041
CaptainEFFF <davidbanville4@gmail.com>
2019-09-05 20:28:29 -0400

    updad styles

--- client/src/components/Game/index.js
+++ client/src/components/Game/index.js
@@ -384,6 +384,8 @@ class Game extends Component {
             <br/>
             <br/>
             <br/>
+            <br/>
+            <br/>
             <span className="bk-page-close" onClick = {this.props.handleClose}>X</span>
             </nav>
 
